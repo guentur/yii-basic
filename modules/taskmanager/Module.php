50577ab3b8ddbc22ece09ebcc2c8b0feb60d6453
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2024-01-25 11:57:54 +0200

    Add taskmanager module

---
+++ modules/taskmanager/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\taskmanager;
+
+/**
+ * taskmanager module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\taskmanager\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
